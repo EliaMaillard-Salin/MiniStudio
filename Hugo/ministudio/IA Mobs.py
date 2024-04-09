a57f97d0d4f2a7db5509e96ed342e9c9dcfed2ae
FasterNight <147041418+FasterNight@users.noreply.github.com>
2024-04-09 10:10:26 +0200

    09/04 - 10h10

--- Hugo/ministudio/IA Mobs.py
+++ Hugo/ministudio/IA Mobs.py
@@ -1,12 +1,56 @@
 import pygame
 import sys
+import PlayerMovement as Player
+import PlatformsClass as Platforms
+
 
 
 # Initialisation de Pygame
 pygame.init()
 
+screen_width = 800
+screen_height = 600
+screen = pygame.display.set_mode((screen_width,screen_height))
+clock = pygame.time.Clock()
+
+player = Player.Player(300,200,50,50)
+ground  = pygame.Rect(0,400,500,100)
+
+allPlateforms=[]
+
+
+P1 = Platforms.Plateform(300, 475, 400, 20, "green",False)
+P1.CreatePlateform(allPlateforms)
+
+P2 = Platforms.Plateform(0, 305, 200, 20, "green",False)
+P2.CreatePlateform(allPlateforms)
+
+P3 = Platforms.Plateform(600, 400, 200, 20, "green",False)
+P3.CreatePlateform(allPlateforms)
+
+P4 = Platforms.Plateform(200, 330, 200, 20, "green",False)
+P4.CreatePlateform(allPlateforms)
+
 
 def collision(rectA: pygame.rect.Rect, rectB: pygame.rect.Rect):
+    #0 None
+    #1 Left
+    #2 Top
+    #3 Bottom
+    #4 Right
+
+    if rectA.left > rectB.right:
+        return 0
+    
+    if rectA.right < rectB.left:
+        return 0
+
+    if rectA.top > rectB.bottom:
+        return 0
+
+    if rectA.bottom < rectB.top:
+        return 0
+
      #calculer les 4 distanes, on stockera dans un tableau de 4 cases
     distance = list(range(0))    
     distance.append(rectA.right - rectB.left) #0
@@ -16,41 +60,17 @@ def collision(rectA: pygame.rect.Rect, rectB: pygame.rect.Rect):
     print(distance)
    
     #on parcourt le tableau, on cherche la distance la plus grande
-    smallest = 0
+    smallest = 1000
     for i in range(len(distance)) :
         if distance[i] < smallest :
-            smallest = i
-       
+            smallest = distance[i]
+            index = i
+            print(index)
 
     #on retourne la face
-    for i in range(len(distance)):
-        if smallest == distance[i]:
-            return i 
-        
-
-        
-    if rectA.left > rectB.right:
-        return 0
-    
-    if rectA.right > rectB.left:
-        return 0
-
-    if rectA.top > rectB.bottom:
-        return 0
-
-    if rectA.bottom < rectB.top:
-        return 0
+    return index + 1
 
 
-  
-
-    
-
-
-# Paramètres de la fenêtre
-screen_width = 800
-screen_height = 600
-screen = pygame.display.set_mode((screen_width, screen_height))
 
 # Couleurs
 black = (0, 0, 0)
@@ -105,127 +125,82 @@ clock = pygame.time.Clock()
 # Boucle de jeu
 running = True
 while running:
-    clock.tick(60)
 
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            running = False
-        elif event.type == pygame.KEYDOWN and event.key == pygame.K_SPACE:
-            # Autoriser le saut uniquement si le joueur n'est pas déjà en train de sauter
-            if not is_jumping:
-                is_jumping = True
-                vertical_velocity = -jump_force
+  
+  clock.tick(60)
+    
+for event in pygame.event.get():
+    if event.type == pygame.QUIT:
+        running = False
+    elif event.type == pygame.KEYDOWN and event.key == pygame.K_SPACE:
+        # Autoriser le saut uniquement si le joueur n'est pas déjà en train de sauter
+        if not player.isJumping:
+            player.isJumping = True
+            player.verticalVelocity = -player.jumpForce
 
-    keys = pygame.key.get_pressed()
+player.Movement()
 
-    # Gestion du dash
-    if keys[pygame.K_LSHIFT] and not is_dashing and dash_cooldown <= 0:
-        is_dashing = True
-        dash_direction = 0
-        if keys[pygame.K_q]:
-            dash_direction = -1
-        elif keys[pygame.K_d]:
-            dash_direction = 1
-        dash_cooldown = 60
+     
+for i in allPlateforms: 
+    if(i.CheckCollision(player.playerRect, player.maxValues)):
+        player.PlayerOnGround(i.Rect.top) 
 
-    if is_dashing:
-        player_x += dash_direction * dash_velocity
-        dash_cooldown -= 1
-        gravity = 0
-        if dash_cooldown <= 50:
-            gravity = 0.5
-            is_dashing = False
+player.CheckWalls()
 
-    if not is_dashing:
-        # Déplacement horizontal
-        if keys[pygame.K_q]:
-            player_x -= player_velocity
-        if keys[pygame.K_d]:
-            player_x += player_velocity
+if player.posY + player.height >= screen_height :
+        player.posX = 300
+        player.posY = 200
 
+# Déstruction d'un bot en sautant dessus
+bot_rect = pygame.Rect(bot_x,bot_y,bot_width,bot_height)
+player_rect = pygame.Rect(player_x, player_y, player_width, player_height)
+col = collision(player_rect,bot_rect)
+print(col)
+if col == 2 :
+    checkpoint1 = False
+    checkpoint2 = False
+    bot_height = 0
+    bot_width = 0
+    bot_x = -1111111
+    bot_y = -1111111
+    pv -= 1
+    vertical_velocity = -jump_attack
+if col == 1 or col == 4 or col == 3:
+    player_height = 0
+    player_width = 0
     
 
 
-    if checkpoint1:
-        bot_x -= bot_velocity
-    if checkpoint2:
-        bot_x += bot_velocity
-  
 
+       
 
-    # Appliquer la gravité
-    vertical_velocity += gravity
-    player_y += vertical_velocity
-
-
-    # Déstruction d'un bot en sautant dessus
-    bot_rect = pygame.Rect(bot_x,bot_y,bot_width,bot_height)
-    player_rect = pygame.Rect(player_x, player_y, player_width, player_height)
-    col = collision(player_rect,bot_rect)
-    if col == 2 :
+# Vérifier la collision avec le checkpoint 1 pour bot
+for plateforms in check:
+    check_rect = pygame.Rect(50,550,20,20)
+    if bot_rect.colliderect(check_rect):
         checkpoint1 = False
-        checkpoint2 = False
-        bot_height = 0
-        bot_width = 0
-        bot_x = -1111111
-        bot_y = -1111111
-        pv -= 1
-        vertical_velocity = -jump_attack
-   
-
-       
-       
-
-    # Vérifier la collision avec le checkpoint 1 pour bot
-    for plateforms in check:
-        check_rect = pygame.Rect(50,550,20,20)
-        if bot_rect.colliderect(check_rect):
-            checkpoint1 = False
-            checkpoint2 = True
+        checkpoint2 = True
 
 
             
-    # Vérifier la collision avec le checkpoint 1 pour bot
-    for plateforms in check1:
-        check_rect = pygame.Rect(750,550,20,20)
-        if bot_rect.colliderect(check_rect):
-            checkpoint2 = False
-            checkpoint1 = True
+# Vérifier la collision avec le checkpoint 1 pour bot
+for plateforms in check1:
+    check_rect = pygame.Rect(750,550,20,20)
+    if bot_rect.colliderect(check_rect):
+        checkpoint2 = False
+        checkpoint1 = True
 
-    # Vérifier la collision avec les plateformes
-    for platform in platforms:
-        platform_rect = pygame.Rect(platform)
-        if player_rect.colliderect(platform_rect) and vertical_velocity > 0:
-            player_y = platform_rect.top - player_height
-            vertical_velocity = 0
-            is_jumping = False  # Réinitialiser l'état de saut à l'atterrissage
 
-    if not is_dashing:
-        dash_cooldown -= 1
 
-    # Affichage
-    screen.fill(black)
-    for platform in platforms:
-        pygame.draw.rect(screen, green, platform)
+# Affichage
+screen.fill("black")
+
+for i in allPlateforms:
+        i.Display(screen)
+
+player.UpdatePlayer(screen)
     
-    if pv == 1:
-        pygame.draw.rect(screen, bleu, (bot_x, bot_y, bot_width, bot_height))
-
-    pygame.draw.rect(screen, black,  plateforms)
-    pygame.draw.rect(screen, black,  plateforms)
-
-    if hp == 1:
-            pygame.draw.rect(screen, white, (player_x, player_y, player_width, player_height))
-
-
-
-
-
-
-
-
-
-    pygame.display.update()
+pygame.display.update()
 
 pygame.quit()
 sys.exit()
