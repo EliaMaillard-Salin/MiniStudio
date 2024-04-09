acdd2cb5ac190237d0c22531152b8171db4ecb59
Kossouurio <ilankonan7@gmail.com>
2024-04-09 16:31:02 +0200

    noa-br

--- Noa/game.py
+++ Noa/game.py
@@ -1,12 +1,11 @@
 import pygame
-import json
-from PlayerMovement import *
-from Plateform import *
+import csv
+import PlayerFiles.PlayerMovement as Player
+import PlatformsFiles.PlatformsClass as Platforms
 
-# Initialisation de Pygame
+
 pygame.init()
 
-# Paramètres de la fenêtre
 SCREEN_WIDTH = 800
 SCREEN_HEIGHT = 640
 screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
@@ -19,24 +18,32 @@ BG_COLOR = (144, 201, 120)
 # Paramètres du jeu
 TILE_SIZE = 40  # Assure-toi que cette valeur correspond à celle utilisée dans l'éditeur
 
-def load_level(level_path='level.json'):
+#load images
+pine1_img = pygame.image.load('asset/img/Background/pine1.png').convert_alpha()
+pine2_img = pygame.image.load('asset/img/Background/pine2.png').convert_alpha()
+mountain_img = pygame.image.load('asset/img/Background/mountain.png').convert_alpha()
+sky_img = pygame.image.load('asset/img/Background/sky_cloud.png').convert_alpha()
+tile_list = [pine1_img, pine2_img, mountain_img, sky_img]
+#############################################################################################
+def load_level(level_path):
     with open(level_path, 'r') as file:
-        return json.load(file)
+        return list(csv.reader(file))
 
 def create_platforms(level_data):
     platforms = []
     for y, row in enumerate(level_data):
         for x, tile_type in enumerate(row):
             if tile_type != -1:  # -1 signifie pas de plateforme
-                platforms.append(Plateform(x * TILE_SIZE, y * TILE_SIZE, TILE_SIZE, TILE_SIZE, WHITE))
+                platforms.append(Platforms.Plateform(x * TILE_SIZE, y * TILE_SIZE, TILE_SIZE, TILE_SIZE, WHITE, False))
     return platforms
 
 # Charge le niveau
-level_data = load_level()
+loading_level_data = load_level("level0_data.csv")
+level_data = [[int(x) for x in inner] for inner in loading_level_data]
 platforms = create_platforms(level_data)
-
+#############################################################################################
 # Crée le joueur
-player = Player(50, SCREEN_HEIGHT - 130, 50, 50)  # Les valeurs initiales peuvent varier selon ton niveau
+player = Player.Player(50, SCREEN_HEIGHT - 130, 50, 50)  # Les valeurs initiales peuvent varier selon ton niveau
 
 # Paramètres de la boucle de jeu
 clock = pygame.time.Clock()
@@ -60,11 +67,11 @@ while running:
 
     # Met à jour et dessine le joueur
     player.Movement()
-    player.UpdatePlayer(screen)
+    player.DisplayPlayer(screen)
 
     # Gestion des collisions
     for platform in platforms:
-        if platform.CheckCollision(player.playerRect):
+        if platform.CheckCollision(player.playerRect, player.maxValues): # Vérifie si le joueur est en collision avec une plateforme
             player.PlayerOnGround(platform.Rect.y)  # Ajuste cette méthode si nécessaire
 
     pygame.display.flip()  # Met à jour l'écran avec tout ce que nous avons dessiné
