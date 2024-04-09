9c3f77100bc902f2bd79d206b976fa06ff864b61
Fur3k <florianh59320@gmail.com>
2024-04-09 17:25:55 +0200

    Menu sans option

--- Flo/Plateform.py
+++ Flo/Plateform.py
@@ -4,26 +4,32 @@ import pygame
 class Plateform: 
     def __init__(self,posX,posY,width,height,color): 
         self.posX = posX
-        self.posY = posY        
-        self.Rect = pygame.Rect(posX,posY,width,height)
+        self.posY = posY    
+        self.width = width
+        self.height = height    
+        self.Rect = pygame.Rect(posX,posY,self.width,self.height)
         self.color = color
-        self.Ground = pygame.Rect(posX + 1 , posY - 1, width - 2 , 1)
 
     def CreatePlateform(self,plateformeList):
         plateformeList.append(self)
 
     def Display(self,surface): 
         pygame.draw.rect(surface,self.color, self.Rect)
-        pygame.draw.rect(surface,"red",self.Ground)
 
     def PlateformCollision(self, Rect):
-        if (self.Ground.left + self.Ground.width >= Rect.left and 
-            self.Ground.left <= Rect.left + Rect.width and
-            self.Ground.top + self.Ground.height >= Rect.bottom - 10  and 
-            self.Ground.top <= Rect.top + Rect.height): #Collision avec le sol  
+        if (self.Rect.left + self.Rect.width >= Rect.left and 
+            self.Rect.left <= Rect.left + Rect.width and
+            self.Rect.top + self.Rect.height >= Rect.bottom - 10  and 
+            self.Rect.top <= Rect.top + Rect.height): #Collision avec le sol  
             return True
         return False
 
+    def update(self, screenPos : tuple[int,int], screenSize : tuple[int,int]):
+        self.posX = screenPos[0]
+        self.posY = screenPos[1]
+        self.width = screenSize[0]
+        self.height = screenSize[1]    
+        self.Rect = pygame.Rect(self.posX,self.posY,self.width,self.height)
 
     def CheckCollision(self, Rect): 
         if self.PlateformCollision(Rect) :
