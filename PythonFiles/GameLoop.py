f198bc91d62e39729177d1e47125faccff1d38cb
PASCAL Noa <noapascal539@gmail.com>
2024-04-10 18:37:26 +0200

    système de pièces
    ajout d'un système de pièce pour point de culture

--- PythonFiles/GameLoop.py
+++ PythonFiles/GameLoop.py
@@ -1,13 +1,16 @@
 import pygame
 import PlayerFiles.PlayerMovement as Player
 import PlatformsFiles.PlatformsClass as Platforms
+import CameraFiles.Camera as Camera
+import PropsFiles.Props as Prop
+import coinsManager
 
 
 pygame.init()
 
 
-screen_width = 800
-screen_height = 600
+screen_width = 1920
+screen_height = 1080
 screen = pygame.display.set_mode((screen_width,screen_height))
 clock = pygame.time.Clock()
 
@@ -26,13 +29,11 @@ P1.CreatePlateform(allPlateforms)
 P2 = Platforms.Plateform(0, 305, 200, 20, "green",False)
 P2.CreatePlateform(allPlateforms)
 
-P3 = Platforms.Plateform(600, 400, 200, 20, "green",False)
-P3.CreatePlateform(allPlateforms)
-
-P4 = Platforms.Plateform(200, 330, 200, 20, "red",True)
+P3 = Platforms.Plateform(600, 450, 200,20, "green", False)
+P4 = Platforms.Plateform(200, 310, 200, 20, "green",False)
 P4.CreatePlateform(allPlateforms)
 
-P5 = Platforms.Plateform(250, 380, 50, 20, "red",True)
+P5 = Platforms.Plateform(320, 380, 200, 20, "red",True)
 P5.CreatePlateform(allPlateforms)
 
 
@@ -41,12 +42,35 @@ P6.CreatePlateform(allPlateforms)
 
 
 
+backGround = pygame.image.load("PythonFiles/Assets/PNG/BackGround.png")
 
+
+allProps = []
+
+
+square = Prop.Props(True,False,20,150,50,50,True,1,0,False)
+allProps.append(square)
+
+food = Prop.Props(False,True,650,300,50,50,False,1,1,False)
+allProps.append(food)
+
+coinA = Prop.Props(False, False, 650,350,50,50, False, 0, 4, True)
+coinB = Prop.Props(False, False, 550,450,50,50, False, 0, 5, True)
+coinC = Prop.Props(False, False, 750,500,50,50, False, 0, 6, True)
+
+allProps.append(coinA)
+allProps.append(coinB)
+allProps.append(coinC)
+
+cam = Camera.Camera(screen_width,screen_height,player.playerVelocity)
 
 while running: 
 
-    clock.tick(60)
+    Pause = False
     
+    dt = clock.tick(60)
+    dt /= 1000
+
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
@@ -55,30 +79,91 @@ while running:
             if not player.isJumping:
                 player.isJumping = True
                 player.verticalVelocity = -player.jumpForce
+        if event.type == pygame.MOUSEBUTTONDOWN and event.button == 1 :
+            player.Attack() 
+            player.LooseOrWinHP(-1)
+            
 
-    player.Movement()
+    player.Movement(dt)
 
-     
+    cam.CamFollow(player, dt)
+
+    # left, right, top, bottom
+    sides = [(-1, 0), (1, 0), (0, -1), (0, 1)]
+
+
     for i in allPlateforms: 
-        if(i.CheckCollision(player.playerRect)):
-            player.PlayerOnGround(i.Rect.top) 
-        if i.solidity: 
-            if(i.CheckWalls(player)) :
-                player.PlayerOnGround(i.Rect.top)
 
+        side, distance = i.GetCollision(player.playerRect)
 
-    if player.posY + player.height >= screen_height :
-        player.posX = 300
-        player.posY = 200
+        if side == 0:
+            continue
 
+        side -= 1
 
+        if side == 3 and i.solidity == False:
+            continue
+
+        direction = sides[side]
+        player.posX += direction[0] * distance
+        player.posY += direction[1] * distance
+
+        if side == 2:
+            player.PlayerOnGround(i.Rect.top)
+            
+    for j in allProps: 
+        j.Collider(player)
     # Affichage
-    screen.fill("black")
+
+    screen.blit(backGround, (0,0))
 
     for i in allPlateforms:
-        i.Display(screen)
+        i.Display(screen, [cam.pos_cam_x, cam.pos_cam_y ] )
 
-    player.DisplayPlayer(screen)
+
+    for i in allProps:
+        
+        if i.coinReveal == True : 
+            Pause = True
+            player.onPause = True
+            cam.onPause = True
+            player.verticalVelocity = 0
+            player.playerVelocity = 0
+            coin = coinsManager.coins(i.index-3)
+            player.isDashing = False
+            coin.show(screen)
+            for eventMenu in pygame.event.get():
+                if eventMenu.type == pygame.KEYDOWN:
+                    if eventMenu.key == pygame.K_ESCAPE and coin.coin != 0:
+                        displayImage = False
+                        player.onPause = False
+                        cam.onPause = False
+                        player.saveY = 0
+                        i.coinReveal = False
+                        player.posY = i.posY
+                        allProps.remove(i)
+                        coin.coin = 0
+                
+            pygame.display.update()
+
+            
+        elif Pause == False:
+            
+            i.DisplayProp(screen, [cam.pos_cam_x, cam.pos_cam_y ] )
+            
+    if Pause == False: 
+        player.DisplayPlayer(screen, cam)
+        player.playerVelocity = 300
+
+        # UI
+        count = 0
+
+        for i in player.listHP: 
+            screen.blit(i, (screen_width - 70 - (count*60), 20))
+            count +=1
+        screen.blit(player.dashImages[player.dashState], (screen_width - 240 , 100 ))
+        
+
     
     pygame.display.update()
 
--- PythonFiles/PlatformsFiles/PlatformsClass.py
+++ PythonFiles/PlatformsFiles/PlatformsClass.py
@@ -1,79 +1,60 @@
 
 import pygame
 
+
 class Plateform: 
      
     def __init__(self,posX,posY,width,height,color,solidity): 
         self.solidity = solidity
         self.posX = posX
-        self.posY = posY        
+        self.posY = posY   
+        self.width = width
+        self.height = height     
         self.Rect = pygame.Rect(posX,posY,width,height)
         self.color = color
+
+
 
     def CreatePlateform(self,plateformeList):
         plateformeList.append(self)
 
-    def Display(self,surface): 
-        pygame.draw.rect(surface,self.color, self.Rect)
+    def Display(self,surface, camPos : tuple[int,int]): 
+        display_rect = pygame.Rect(self.posX - camPos[0], self.posY - camPos[1], self.width, self.height )
+        pygame.draw.rect(surface,self.color, display_rect)
 
-    def PlateformCollision(self, Rect):
-        if (self.Rect.left + self.Rect.width >= Rect.left and 
-            self.Rect.left <= Rect.left + Rect.width and
-            self.Rect.top + self.Rect.height >= Rect.bottom - 10  and 
-            self.Rect.top <= Rect.top + Rect.height): #Collision avec le sol  
 
-            return True
+    def GetCollision(self, rect: pygame.rect.Rect) -> tuple[int, int]:
+        #0 => None
+        #1 => Left
+        #2 => Right
+        #3 => Top
+        #4 => Bottom 
+
+        if rect.right < self.Rect.left:
+            return 0, 0
         
-        return False
-    
-
-    def PlateformMove(self,cam,player ):
-        if cam.oldPosX < cam.pos_cam_x and cam.pos_cam_x < cam.limit_right: 
-            self.posX -= player.playerVelocity
-        if cam.oldPosX > cam.pos_cam_x and cam.pos_cam_x > cam.limit_left:
-            self.posX += player.playerVelocity
-
-        if cam.oldPosY < cam.pos_cam_y :
-            self.posY -= cam.pos_cam_y - cam.oldPosY
-        if cam.oldPosY > cam.pos_cam_y : 
-            self.posY += cam.oldPosY - cam.pos_cam_y
-
-        self.Rect = pygame.Rect(self.posX,self.posY,self.width,self.height)
-
-
-    def SetMaxValue(self, objectRect, maxvalue : list[int]): 
-
-        if (self.Rect.left + self.Rect.width > objectRect.left) and  (self.Rect.left < objectRect.left + objectRect.width ):
-            if (self.Rect.bottom <= objectRect.top): 
-                maxvalue[0] = self.Rect.bottom
-            else : 
-                maxvalue[0] = -1
-            if (self.Rect.top >= objectRect.top + objectRect.height): 
-                maxvalue[1] = self.Rect.top
-            else : 
-                maxvalue[1] = -1
-        else : 
-            maxvalue[0] = -1 ; maxvalue[1] = -1
-        if ( self.Rect.top + self.Rect.height > objectRect.top) and (self.Rect.top <  objectRect.top +objectRect.height):
-            if(self.Rect.right<= objectRect.left):
-                maxvalue[2] = self.Rect.right   
-            else : 
-                maxvalue[2] = -1
-            if (self.Rect.left>= objectRect.right): 
-                maxvalue[3] = self.Rect.left   
-            else : 
-                maxvalue[3] = -1 
-        else : 
-            maxvalue[2] = -1 ; maxvalue[3] = -1
-
-
-    def CheckCollision(self, Rect, maxvalue: list[int]): 
-        if self.solidity : 
-            self.SetMaxValue(Rect, maxvalue)
-        else : 
-            if self.PlateformCollision(Rect) :
-                return True
-            return False
+        if rect.left > self.Rect.right:
+            return 0, 0
         
-        return False
-    
+        if rect.top > self.Rect.bottom:
+            return 0, 0
+        
+        if rect.bottom < self.Rect.top:
+            return 0, 0
+
+        distances: list[int] = []
+        distances.append(abs(rect.right - self.Rect.left))
+        distances.append(abs(rect.left - self.Rect.right))
+        distances.append(abs(rect.bottom - self.Rect.top))
+        distances.append(abs(rect.top - self.Rect.bottom))
+
+        minDistance = 2000
+        side: int = 0
+        for i in range(0, len(distances)):
+            if minDistance > distances[i]:
+                minDistance = distances[i]
+                side = i + 1
+        
+        return side, minDistance
+
+
